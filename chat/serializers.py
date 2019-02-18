471e2842ccd358ad20d2cea4b3705863891542a7
Paweł Staś <pawelstas@wp.pl>
2019-02-18 22:11:44 +0100

    Changed message field to read_only

--- chat/serializers.py
+++ chat/serializers.py
@@ -4,6 +4,8 @@ from rest_framework import serializers
 from chat.models import Room, Message
 
 class RoomSerializer(serializers.ModelSerializer):
+    messages = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Room
         fields = ('id', 'name', 'messages')
