f8aef00bb388a83584374fe87c0ed6f276cbf299
Paweł Staś <pawelstas@wp.pl>
2019-03-25 13:42:08 +0100

    Added TimestampField for MessageSerializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,16 +1,20 @@
 # pylint: disable=no-member
 
+import time
 from rest_framework import serializers
 from chat.models import Room, Message
 
-class RoomSerializer(serializers.ModelSerializer):
-    messages = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+class TimestampField(serializers.Field):
+    def to_representation(self, value):
+        return int(time.mktime(value.timetuple()))
 
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('id', 'name', 'messages')
+        fields = ('id', 'name')
 
 class MessageSerializer(serializers.ModelSerializer):
+    created = TimestampField()
     class Meta:
         model = Message
         fields = ('id', 'text', 'author', 'created', 'room')
