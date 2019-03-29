425a90589b82c8462a70561a65f74f1e01029c8f
Paweł Staś <pawelstas@wp.pl>
2019-03-29 12:24:45 +0100

    Added to_internal_value method

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,12 +1,16 @@
 # pylint: disable=no-member
 
 import time
+import datetime
 from rest_framework import serializers
 from chat.models import Room, Message
 
 class TimestampField(serializers.Field):
     def to_representation(self, value):
         return int(time.mktime(value.timetuple()))
+
+    def to_internal_value(self, value):
+        return datetime.datetime.utcfromtimestamp(value)
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
